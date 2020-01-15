2dc7e1bc1aad52103a1eae3205d830c989a3d39d
Code4Blessings <robinwarden@code4blessings.com>
2020-01-15 15:11:20 -0500

    Created Server.js file, moved index.js contents to it, and connected them

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const express = require('express');
+const server = require('./api/server')
 
-const dBase = require('./data/db');
-
-const server = express();
-
-server.use(express.json());
-
-server.use('/', (req, res) => {
-    res.send({
-        Greeting: "Hello again from the dBase!!!"
-    })
-})
 
 server.listen(4000, () => console.log('API is running on Port 4000'));
